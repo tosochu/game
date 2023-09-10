afa987c1d65960b12cab92eeda66b6832403fb8a
Milmon <107737161+Molmin@users.noreply.github.com>
2023-09-10 16:21:49 +0800

    fix tabs

--- assets/script/user.js
+++ assets/script/user.js
@@ -1,34 +1,33 @@
 $(document).ready(() => {
-    $('.page-userLogin .login').click(async () => {
-      if (!$('.username').val() || !$('.password')) return;
-      var response = await fetch("/api/user/login", {
-        "headers": { "content-type": "application/json" },
-        "body": JSON.stringify({
-          username: $('.username').val(),
-          password: $('.password').val(),
-        }),
-        "method": "POST",
-      });
-      var result = await response.json();
-      if (result.error) window.alert(result.error);
-      else window.location.pathname = '/';
+  $('.page-userLogin .login').click(async () => {
+    if (!$('.username').val() || !$('.password')) return;
+    var response = await fetch("/api/user/login", {
+      "headers": { "content-type": "application/json" },
+      "body": JSON.stringify({
+        username: $('.username').val(),
+        password: $('.password').val(),
+      }),
+      "method": "POST",
     });
-  
-    $('.page-userRegister .register').click(async () => {
-      if (!$('.username').val() || !$('.password').val()) return;
-      if ($('.password').val() != $('.repeat').val())
-        return window.alert('两次输入的密码不一致。');
-      var response = await fetch("/api/user/register", {
-        "headers": { "content-type": "application/json" },
-        "body": JSON.stringify({
-          username: $('.username').val(),
-          password: $('.password').val(),
-        }),
-        "method": "POST",
-      });
-      var result = await response.json();
-      if (result.error) window.alert(result.error);
-      else window.location.pathname = '/login';
-    });
+    var result = await response.json();
+    if (result.error) window.alert(result.error);
+    else window.location.pathname = '/';
   });
-  
+
+  $('.page-userRegister .register').click(async () => {
+    if (!$('.username').val() || !$('.password').val()) return;
+    if ($('.password').val() != $('.repeat').val())
+      return window.alert('两次输入的密码不一致。');
+    var response = await fetch("/api/user/register", {
+      "headers": { "content-type": "application/json" },
+      "body": JSON.stringify({
+        username: $('.username').val(),
+        password: $('.password').val(),
+      }),
+      "method": "POST",
+    });
+    var result = await response.json();
+    if (result.error) window.alert(result.error);
+    else window.location.pathname = '/login';
+  });
+});
